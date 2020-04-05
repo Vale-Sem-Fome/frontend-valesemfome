2b1b5ed301e735512f30a296827e82edab44715f
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2020-04-05 18:05:09 -0300

    fix: bugs pages. add thanks page.

--- vale-sem-fome/src/pages/Obrigado/index.js
+++ vale-sem-fome/src/pages/Obrigado/index.js
@@ -1,9 +1,29 @@
 import React from 'react'
+import { Images } from '../../components'
 
 export default function Obrigado() {
+    const images = [
+        "/images/instagram_pics/01-sjc-sem-fome.jpg",
+        "/images/instagram_pics/02-sjc-sem-fome.jpg",
+        "/images/instagram_pics/04-sjc-sem-fome.jpg"
+    ]
+
     return (
-        <div>
-            <h1>Obrigado!</h1>
+        <div className="container">
+            <main className="thanks-page">
+                <div className="thanks-container">
+                    <p>Obrigada por nos contar um pouco mais sobre você e sua família!</p>
+                    <h1>Você é muito importante para nós!</h1>
+                    <p>
+                        <strong>Lembrando:</strong> este cadastro não garante o 
+                        recebimento dos alimentos, mas vamos 
+                        nos esforçar ao máximo para atender a todos. 
+                    </p>
+                    <div className="img-container">
+                        <Images images={images} />
+                    </div>
+                </div>
+            </main>
         </div>
     )
 }
