2861d20ccdf1790e056b60e7c548803888153636
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2020-04-05 21:24:13 -0300

    fix: url axios

--- vale-sem-fome/src/services/api.js
+++ vale-sem-fome/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Api = axios.create({
-    baseURL: 'https://cors-anywhere.herokuapp.com/https://saojosesemfome.appspot.com/api/v1.0/sjcsemfome'
+    baseURL: 'https://saojosesemfome.appspot.com/api/v1.0/sjcsemfome'
 });
 
 export default Api;
