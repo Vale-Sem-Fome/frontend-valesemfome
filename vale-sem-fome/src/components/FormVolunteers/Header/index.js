b0a626de4ab8f41f1425281e6f2e912fd477ffe3
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2020-04-08 00:15:46 -0300

    fix: bugs form and header volunteers

---
+++ vale-sem-fome/src/components/FormVolunteers/Header/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import logo from '../../../assets/images/logo.png'
+
+export default function Header() {
+    return (
+        <header className="header">
+            <div className="container">
+                <div className="logos">
+                    <img className="img-logo" src={logo} alt="sjc sem fome e regeneração global" />
+                </div>
+                <section>
+                    <div className="text">
+                        <h1>
+                            CADASTRO DE VOLUNTÁRIOS PARA KITS E ENTREGAS
+                            MOVIMENTO COVID19 SJC SEM FOME
+                        </h1>
+                        <p>
+                            <strong>Importante:</strong> Precisamos de ajuda de novos voluntários para 
+                            montagens dos kits de alimentos e também para realização de 
+                            entregas porta a porta.
+                        </p>
+                    </div>
+                </section>
+                <div className="fake-button">
+                    <a href="#form">Começar!</a>
+                </div>
+            </div>
+        </header>
+    )
+}
