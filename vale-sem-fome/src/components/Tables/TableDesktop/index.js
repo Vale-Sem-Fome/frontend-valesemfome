98f8a92d57f3756a4c695a5c25e1d4645562944e
Vanessa Ribeiro <vanribeiro.dev@gmail.com>
2020-04-18 03:32:31 -0300

    feat: allow to have a desktop and mobile table.

--- vale-sem-fome/src/components/Tables/TableDesktop/index.js
+++ vale-sem-fome/src/components/Tables/TableDesktop/index.js
@@ -1,102 +1,28 @@
 import React from 'react';
 import './table-desktop.scss';
 
-function TableDesktop(){
+function TableDesktop({fullName, cpf, 
+    regionCity, neighbourhood,
+    currentFinancialStatus}){
     return (
         <div className="container">
             <table className="table">
                 <thead>
+                    <tr>
                     <th>Nome:</th>
                     <th>CPF:</th>
                     <th>Região</th>
                     <th>Bairro</th>
                     <th>Situação Atual:</th>
+                    </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td>
-                            José Luiz Vasco
-                        </td>
-                        <td>
-                            011.010.001-00
-                        </td>
-                        <td>
-                            Zona Leste
-                        </td>
-                        <td>
-                            Novo Horizonte
-                        </td>
-                        <td>
-                            Extremamente Séria
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                        Sheila Santana Vilela Araújo
-                        </td>
-                        <td>
-                            011.010.001-00
-                        </td>
-                        <td>
-                            Zona Norte
-                        </td>
-                        <td>
-                            Santana
-                        </td>
-                        <td>
-                            Muito Séria
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                            Thaís Okamoto
-                        </td>
-                        <td>
-                            011.010.001-00
-                        </td>
-                        <td>
-                            Zona Oeste
-                        </td>
-                        <td>
-                            Jardim das Indústrias
-                        </td>
-                        <td>
-                            Séria
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                        André Luiz Serena Maciel
-                        </td>
-                        <td>
-                            011.010.001-00
-                        </td>
-                        <td>
-                            Centro
-                        </td>
-                        <td>
-                            Jardim Santa Inês
-                        </td>
-                        <td>
-                            Outros
-                        </td>
-                    </tr>
-                    <tr>
-                        <td>
-                        André Luiz Serena Maciel
-                        </td>
-                        <td>
-                            011.010.001-00
-                        </td>
-                        <td>
-                            Centro
-                        </td>
-                        <td>
-                            Jardim Santa Inês
-                        </td>
-                        <td>
-                            Risco
-                        </td>
+                        <td>{fullName}</td>
+                        <td>{cpf}</td>
+                        <td>{regionCity}</td>
+                        <td>{neighbourhood}</td>
+                        <td>{currentFinancialStatus}</td>
                     </tr>
                 </tbody>
             </table>
--- vale-sem-fome/src/pages/Recipients/ShowAll/index.js
+++ vale-sem-fome/src/pages/Recipients/ShowAll/index.js
@@ -2,17 +2,46 @@ import React from 'react';
 import { 
     CityHeader,
     SearchForm,
+    TableMobile,
+    TableDesktop,
     Footer
 } from './../../../components/index';
 
 function ShowAll() {
-    return (
-        <div>
-            <CityHeader cityName="São José dos Campos"/>
-            <SearchForm totalEntries="23.023 registros"/>
-            <Footer />
-        </div>
-    )
+    let screenWidth = window.screen.width;
+
+    if(screenWidth < 768){
+        return (
+            <div>
+                <CityHeader cityName="São José dos Campos"/>
+                <SearchForm totalEntries="23.023 registros"/>
+                <TableMobile 
+                    fullName="José Luiz Vasco" 
+                    cpf="000.001.010-01"
+                    regionCity="Zona Norte"
+                    neighbourhood="Novo Horizonte"
+                    currentFinancialStatus="Extremamente Séria"
+                />
+                <Footer />
+            </div>
+        )
+    } else{
+        return (
+            <div>
+                <CityHeader cityName="São José dos Campos"/>
+                <SearchForm totalEntries="23.023 registros"/>
+                <TableDesktop 
+                    fullName="José Luiz Vasco" 
+                    cpf="000.001.010-01"
+                    regionCity="Zona Norte"
+                    neighbourhood="Novo Horizonte"
+                    currentFinancialStatus="Extremamente Séria"
+                />
+                <Footer />
+            </div>
+        )
+    }
+
 }
 
 export default ShowAll;
